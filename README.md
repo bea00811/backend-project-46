b192bb1fea8c71b322a507b3d2fe8e1bcca7b781
bea00811 <irinakozlovawork@gmail.com>
2023-04-10 10:07:17 +0300

    3d step whith askinema

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/bea00811/backend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/bea00811/backend-project-46/actions)
+
+<a href="https://asciinema.org/a/fzxmYAin8ZGDSTonQmpDiEced" target="_blank"><img src="https://asciinema.org/a/fzxmYAin8ZGDSTonQmpDiEced.svg" /></a>
+g
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -16,8 +16,6 @@ program
   .argument('filepath1')
   .argument('filepath2')
   .action((filepath1, filepath2) => {
-    console.log('filepath1:', filepath1);
-    console.log('filepath2:', filepath2);
     const data1 = fs.readFileSync(filepath1, { encoding: 'utf8' });
     const data2 = fs.readFileSync(filepath2, { encoding: 'utf8' });
     // Display the file data
@@ -33,74 +31,75 @@ program
     const equalFile1File2 = arr1.filter((item1) => arr2.find((item2) => _.isEqual(item1, item2)));
 
     // ключ есть только в одном файле
-    const diffFile1 = arr1.filter((e) => arr2.findIndex((i) => i[0] == e[0]) === -1);
-    const diffFile2 = arr2.filter((e) => arr1.findIndex((i) => i[0] == e[0]) === -1);
+    const diffFile1 = arr1.filter((e) => arr2.findIndex((i) => i[0] === e[0]) === -1);
+    const diffFile2 = arr2.filter((e) => arr1.findIndex((i) => i[0] === e[0]) === -1);
 
     // значение по ключу отличается
-    const diffOnlyValarr1 = arr1.filter((item1) => arr2.find((item2) => item1[0] === item2[0] && item1[1] !== item2[1]));
-    const diffOnlyValarr2 = arr2.filter((item1) => arr1.find((item2) => item1[0] === item2[0] && item1[1] !== item2[1]));
+    const difValue1 = arr1.filter((item1) => arr2.find((item2) => {
+      if (item1[0] === item2[0] && item1[1] !== item2[1]) {
+        return true;
+      }
+      return false;
+    }));
+    const difValue2 = arr2.filter((item1) => arr1.find((item2) => {
+      if (item1[0] === item2[0] && item1[1] !== item2[1]) {
+        return true;
+      }
+      return false;
+    }));
 
     // Записываем отфильтрованные массивы  в один большой объект
 
     const obj = {};
 
-    const res5 = equalFile1File2.reduce((result, current) => {
+    equalFile1File2.reduce((result, current) => {
       result[`  ${current[0]}`] = `${current[1]}`;
       return result;
     }, obj);
 
-    const res = diffFile1.reduce((result, current) => {
+    diffFile1.reduce((result, current) => {
       result[`- ${current[0]}`] = `${current[1]}`;
       return result;
     }, obj);
 
-    const res2 = diffFile2.reduce((result, current) => {
+    diffFile2.reduce((result, current) => {
       result[`+ ${current[0]}`] = `${current[1]}`;
       return result;
     }, obj);
 
-    const res3 = diffOnlyValarr1.reduce((result, current) => {
+    difValue1.reduce((result, current) => {
       result[`- ${current[0]}`] = `${current[1]}`;
       return result;
     }, obj);
 
-    const res4 = diffOnlyValarr2.reduce((result, current) => {
+    difValue2.reduce((result, current) => {
       result[`+ ${current[0]}`] = `${current[1]}`;
       return result;
     }, obj);
 
-    // Это один большой объект, пока не отфильтрованный
-    console.log(obj);
-    console.log(Object.entries(obj));
+    // Это один большой объект, уже отфильтрованный
 
     const myobj = Object.entries(obj);
-    console.log(myobj);
 
     // Отсортировала объект обратно в массив массивов
     const sortedArr2 = [];
-    for (const [key, value] of myobj) {
-      sortedArr2.push([key, value]);
-    }
-    console.log(sortedArr2);
+    myobj.map(([key, value]) => sortedArr2.push([key, value]));
 
-    // Отсортировала массив массивов в алфавитном порядке и чтобы первый файл шел первым если  ключи равны
-    const sortedArr3 = sortedArr2.sort((a, b) => ((a[0].slice(2) === b[0].slice(2) ? 0 : a[0].slice(2) > b[0].slice(2)) ? 1 : -1));
+    // Отсорт-а массив массивов в алфав порядке и чтобы первый файл шел первым если  ключи равны
 
-    const funcArr = sortedArr2.sort((a, b) => {
+    const sortedArr32 = sortedArr2.sort((a, b) => {
       if (a[0].slice(2) === b[0].slice(2)) {
-        return a > b;
+        return 0;
       }
-
       if (a[0].slice(2) > b[0].slice(2)) {
-        return a > b;
+        return 1;
       }
-      return a < b;
+      return -1;
     });
-    console.log(`newarr ${funcArr}`);
-    console.log(sortedArr3);
+
     // sorted right string is here!!!
-    console.log(JSON.stringify(Object.fromEntries(sortedArr3), null, ' '));
+
+    const compareFiles = JSON.stringify(Object.fromEntries(sortedArr32), null, ' ');
+    console.log(compareFiles);
   })
   .parse(process.argv);
-
-console.log("I'm console log");
