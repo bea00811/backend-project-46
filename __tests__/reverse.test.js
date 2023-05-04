536ff21bafc0597afca34c367e5ead26bf1e0bd2
bea00811 <irinakozlovawork@gmail.com>
2023-05-04 20:12:49 +0300

    99

--- __tests__/reverse.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@jest/globals';
-import reverse from '../bin/reverse.js';
-// const reverse = require('../bin/reverse.js');
-
-test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh');
-  expect(reverse('')).toEqual('');
-});
--- __tests__/sum.test.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@jest/globals';
-import sum from '../bin/sum.js';
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
