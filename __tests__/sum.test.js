febd8e3fb749db9f6ebb5045b354d33cee5bf432
bea00811 <irinakozlovawork@gmail.com>
2023-05-04 20:08:23 +0300

    88

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,6 +1,5 @@
 import { test, expect } from '@jest/globals';
 import sum from '../bin/sum.js';
-// const sum = require('../bin/sum.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
