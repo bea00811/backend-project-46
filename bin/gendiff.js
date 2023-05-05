820aeaab033f171f4e5f18d0397f1387a01c8d89
bea00811 <irinakozlovawork@gmail.com>
2023-05-05 14:41:16 +0300

    1010

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,26 +7,11 @@ import wrapperforcommander from './wrapperforcommander.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format <type>', 'add the specified type of cheese', 'stylish')
-  // .option('-l, --lower', 'only use lowercase letters')
-  // .option('-u, --upper', 'only use uppercase letters')
-  // .option('-f, --format [type]', 'output format')
-  // .option('-s, --stylish', 'only use stylish format')
-  // .option('-u, --plain', 'only use plain format')
+  .option('-f, --format <type>', 'choose the format of diffs', 'stylish')
 
   .argument('filepath1')
   .argument('filepath2')
   .action((filepath1, filepath2) => {
-    // console.log(filepath1.split('.')[1]);
-    // console.log(filepath2.split('.')[1]);
-    // console.log(getFileFormat(data1));
-    // console.log(getFileFormat(data2));
-    // compareFiles(filepath1, filepath2);
-    // const options = program.opts();
-    // const { stylish, plain } = options;
     console.log(wrapperforcommander(filepath1, filepath2, program.opts().format));
-    // console.log(program.opts());
-    // console.log(formatter(getRightTree(data1, data2, program.opts().format)));
-    // console.log(`cheese: ${program.opts().format}`);
   })
-  .parse(process.argv);
+  .parse();
