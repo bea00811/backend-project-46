442648b262a886443a1ab024e79955154bad4894
bea00811 <irinakozlovawork@gmail.com>
2023-05-05 14:45:10 +0300

    11

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,8 @@ import wrapperforcommander from './wrapperforcommander.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'choose the format of diffs', 'stylish')
 
   .argument('filepath1')
