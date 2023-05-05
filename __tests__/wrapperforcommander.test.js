0e72e32c02967f589457b4cb8690165ae0d55a37
bea00811 <irinakozlovawork@gmail.com>
2023-05-05 21:36:50 +0300

    99

--- __tests__/wrapperforcommander.test.js
+++ __tests__/wrapperforcommander.test.js
@@ -10,14 +10,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const data1 = getFixturePath('file1.yml');
-const data2 = getFixturePath('file2.yml');
+const data1 = getFixturePath('file3.yml');
+const data2 = getFixturePath('file4.yml');
 
 /** **some comment1 */
 
 // const rightAnswer1 = fs.readFileSync(getFixturePath('rightStylish.txt'), 'utf8');
-const rightAnswer2 = fs.readFileSync(getFixturePath('result_plain.txt'), 'utf8');
-// const rightAnswer3 = fs.readFileSync(getFixturePath('rightJson.txt'), 'utf8');
+const rightAnswer2 = fs.readFileSync(getFixturePath('rightPlain.txt'), 'utf8');
 
 // test('test forWrapper1', () => {
 //   expect(wrapperforcommander(data1, data2, 'stylish')).toBe(rightAnswer1);
