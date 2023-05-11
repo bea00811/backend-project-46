b6cf8feb6c8da316e1647927768623ed862dd95e
bea00811 <irinakozlovawork@gmail.com>
2023-05-11 14:29:11 +0300

    14:29

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'choose the format of diffs', 'stylish')
-
   .argument('filepath1')
   .argument('filepath2')
   .action((filepath1, filepath2) => {
