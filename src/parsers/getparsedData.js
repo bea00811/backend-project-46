22f74f87725bd3d7b0a60c09b795e405c439dc08
bea00811 <irinakozlovawork@gmail.com>
2023-05-11 16:36:37 +0300

    16:36

--- src/parsers/getparsedData.js
+++ src/parsers/getparsedData.js
@@ -1,9 +1,11 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import parser from './parser.js';
 import getFileFormat from './getFileFormat.js';
 
 const getparsedData = (filepath1) => {
-  const filedata1 = fs.readFileSync(filepath1, { encoding: 'utf8' });
+  const fullPath = path.resolve(process.cwd(), filepath1);
+  const filedata1 = fs.readFileSync(fullPath).toString();
   const format1 = getFileFormat(filepath1);
   const data1 = parser(format1, filedata1);
 
