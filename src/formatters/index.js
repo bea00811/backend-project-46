1c0dcf7a184e4de6209d15ef8e43d9f9a4b724d8
bea00811 <irinakozlovawork@gmail.com>
2023-05-12 20:58:58 +0300

    removed coverage

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -15,9 +15,6 @@ const formatter = (data1, data2, formatoption) => {
     return stylish(getRightTree(data1, data2));
   }
 
-  // if (formatoption === undefined) {
-  //   return stylish(getRightTree(data1, data2));
-  // }
   return new Error(`Dear user, your format ${formatoption} is wrong. Please type correctly.`);
 };
 
