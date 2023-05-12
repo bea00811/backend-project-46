f9c891635f1d0e0fd0eee0bfa0d7554565345d11
bea00811 <irinakozlovawork@gmail.com>
2023-05-12 11:51:37 +0300

    11:51

--- __tests__/wrapperforcommander.test.js
+++ __tests__/wrapperforcommander.test.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as path from 'path';
 
-import compareFiles from '../src/wrapperforcommander.js';
+import compareFiles from '../bin/wrapperforcommander.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
